91b074bb344423f084de99d942c4ca15cc90a4fc
Mallina Sandeep <msandeep@itron.com>
2024-09-03 13:12:43 +0530

    Add project files.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TrainingManagementSystem.Models;
+
+namespace TrainingManagementSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly TMSdbContext _context;
+        public LoginController(TMSdbContext context)
+        {
+            _context = context;
+
+        }
+
+        [HttpGet]
+        [Route("/login")]
+        public IActionResult Get()
+        {
+            return Ok(_context.Employees.ToList());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+
+using Microsoft.EntityFrameworkCore;
+using TrainingManagementSystem.Models;
+namespace TrainingManagementSystem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddDbContext<TMSdbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("TmsDB"));
+            });
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
